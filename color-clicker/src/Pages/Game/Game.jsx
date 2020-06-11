42d51869bdf7e561b3f057ee0bea99e0cbeda54c
Дмитрий <burydima28122003@gmail.com>
2020-06-11 20:10:13 +0300

    basic styles at ClickerButton

--- color-clicker/src/Pages/Game/Game.jsx
+++ color-clicker/src/Pages/Game/Game.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ClickerButton } from '../../Components/ClickerButton/ClickerButton';
 
-export function Game() {
-  return (
-    <div >
-     
-    </div>
-  );
+const Wrap = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  margin: 0;
+  padding: 0;
+`
+
+export function Game(props) {
+
+  const [active, setActive] = useState(false);
+
+    return (
+      <Wrap >
+        <ClickerButton active={true}/>
+        <ClickerButton active={false}/>
+        <ClickerButton active={false}/>
+        <ClickerButton active={false}/>
+
+      </Wrap>
+    );
 }
